7c9c664ef6c3d0b061354edb15e25596e5674a3f
zzxx552 <86451335+zzxx552@users.noreply.github.com>
2021-06-25 15:48:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git helloworld https://github.com/DD663663/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/zzxx552/helloworld' feeds.conf.default
